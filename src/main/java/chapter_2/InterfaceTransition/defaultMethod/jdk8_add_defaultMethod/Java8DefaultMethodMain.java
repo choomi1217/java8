8c285210446a85f4e3f105207c794d0fb318a8ca
o_omi <whdudal1217@naver.com>
2022-10-10 22:06:09 +0900

    :fire: 메인 주석

--- src/main/java/chapter_2/InterfaceTransition/defaultMethod/jdk8_add_defaultMethod/Java8DefaultMethodMain.java
+++ src/main/java/chapter_2/InterfaceTransition/defaultMethod/jdk8_add_defaultMethod/Java8DefaultMethodMain.java
@@ -8,7 +8,7 @@ import java.util.Spliterator;
 import java.util.stream.Collectors;
 
 public class Java8DefaultMethodMain {
-
+/*
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
         names.add("a123");
@@ -16,39 +16,35 @@ public class Java8DefaultMethodMain {
         names.add("c123");
         names.add("d123");
 
-        /*
         names.forEach(System.out::println);
-        */
 
-        /*
         Spliterator<String> spliterator = names.spliterator();
         Spliterator<String> trySplit = spliterator.trySplit();
         while (spliterator.tryAdvance(System.out::println));
         System.out.println("----------------------------");
         while (trySplit.tryAdvance(System.out::println));
-        */
 
-        /* stream count */
+        // stream count
         long count = names.stream().map(String::toUpperCase)
             .filter(s -> s.startsWith("A"))
             .count();
         System.out.println(" count : " + count);
 
-        /* stream to set */
+        // stream to set
         Set<String> set = names.stream().map(String::toUpperCase)
             .filter(s -> s.startsWith("A"))
             .collect(Collectors.toSet());
         System.out.println(" set : " + set);
 
-        /* collection removeIf */
+        // collection removeIf
         names.removeIf(s -> s.startsWith("a"));
         names.forEach(System.out::println);
 
-        /* compare */
+        // compare
         Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
         names.sort(compareToIgnoreCase.reversed());
         names.forEach(System.out::println);
 
     }
-
+*/
 }
