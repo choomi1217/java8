c5aa433f6c6dba7cd1493c5fcea996e42f2dbda5
o_omi <whdudal1217@naver.com>
2022-10-26 22:55:26 +0900

    :sparkles: 자바 8 Executor

---
+++ src/main/java/chapter_6/CompletableFuture/Executor/ExecutorMain.java
@@ -0,0 +1,18 @@
+package chapter_6.CompletableFuture.Executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorMain {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread : " + Thread.currentThread().getName());
+            }
+        });
+    }
+
+}
