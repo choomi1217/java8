d4284e7ffeda44d16219593bcc1ebb0fd9a5f0b7
o_omi <whdudal1217@naver.com>
2022-11-01 23:14:53 +0900

    :sparkles: 자바 8 Executor

--- src/main/java/chapter_6/CompletableFuture/Executor/ExecutorMain.java
+++ src/main/java/chapter_6/CompletableFuture/Executor/ExecutorMain.java
@@ -2,17 +2,22 @@ package chapter_6.CompletableFuture.Executor;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorMain {
-
+/*
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Thread : " + Thread.currentThread().getName());
-            }
-        });
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        //executorService.schedule(getRun("hi"),3, TimeUnit.SECONDS);
+
+        executorService.scheduleAtFixedRate(getRun("hi"),3,1, TimeUnit.SECONDS);
+
+    }
+*/
+    public static Runnable getRun(String s){
+        return () -> System.out.println(s + Thread.currentThread().getName());
     }
 
 }
