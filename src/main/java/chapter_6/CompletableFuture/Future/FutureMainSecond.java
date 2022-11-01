964933554c70bed27771bc63437c67061f51260c
o_omi <whdudal1217@naver.com>
2022-11-02 00:20:05 +0900

    :sparkles: 자바 8 CompletableFuture 활용

---
+++ src/main/java/chapter_6/CompletableFuture/Future/FutureMainSecond.java
@@ -0,0 +1,32 @@
+package chapter_6.CompletableFuture.Future;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+public class FutureMainSecond {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        boolean throwError = true;
+
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> {
+            if(throwError){
+                throw  new IllegalArgumentException();
+            }
+            System.out.println("Hello" + Thread.currentThread().getName());
+            return "Hello";
+        }).handle((result, ex) -> {
+            if(ex != null){
+                System.out.println(ex);
+                return "Error";
+            }
+            return result;
+        });
+
+        System.out.println(hello.get());
+
+    }
+}
